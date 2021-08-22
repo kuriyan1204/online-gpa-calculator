331ac26106c1fa561d0877b06cc71b06defd2a5d
Hiroto <hiroto.kurita.q4@dc.tohoku.ac.jp>
2021-08-22 17:58:43 +0900

    Updated Main

--- app.py
+++ app.py
@@ -1,42 +1,44 @@
 import streamlit as st
 import jaconv
 
-
 GRADE = {"AA":4.0,"A":3.0,"B":2.0,"C":1.0,"D":0.0}
 
 def home():
-    st.write("# GPA Calculator for Tohoku University Students (ver.β)")
 
-    lines = st.text_area("Copy & Paste your grades").split("\n")
+    st.write("# 東北大生用 GPA Calculator (β版)")
+    lines = st.text_area("学務情報システムから，成績をコピーしてそのままペーストしてください．",height=300).split("\n")
     
-    new = []
+    grades = []
     for line in lines:
-        separated = line.split()
+        splited = line.split()
         try:
-            new.append([separated[-3],separated[-4]])
+            grades.append([splited[-3],splited[-4]])
         except:
             pass
     
     raw_gpa = 0.0 
+    weighted_gpa = 0.0
     cred = 0.0
 
-    for gr in new:
-        gr[0] = jaconv.z2h(gr[0], kana=False, digit=True, ascii=True)
-        gr[1] = jaconv.z2h(gr[1], kana=False, digit=True, ascii=True)
-        if gr[0] in GRADE.keys():
-            raw_gpa += GRADE[gr[0]]*float(gr[1])
-            cred += float(gr[1])
+    for grade in grades:
+        grade[0] = jaconv.z2h(grade[0], kana=False, digit=True, ascii=True)
+        grade[1] = jaconv.z2h(grade[1], kana=False, digit=True, ascii=True)
+        if grade[0] in GRADE.keys():
+            raw_gpa += GRADE[grade[0]]
+            weighted_gpa += GRADE[grade[0]]*float(grade[1])
+            cred += float(grade[1])
 
-    try:
-        st.write(raw_gpa/cred)
-    except:
-        pass
+    operation = st.button("GPAを計算")
 
-    st.write(raw_gpa)
-    st.write(cred)
-    st.write(lines)
-    st.write(new)
-    return None
+    if not operation:
+        st.stop()
+
+    if not cred:
+        st.write("取得された単位数が0のため，GPAを計算することができません．")
+        st.stop()
+    
+    st.write(f"総取得単位数：{cred}")
+    st.write(f"GPA：{weighted_gpa/cred}")
 
 #deploy sever
 if __name__ == "__main__":
