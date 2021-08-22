fb7949f7273a86692217110a50f28e0164f6f8ea
Hiroto <73727292+kuriyan1204@users.noreply.github.com>
2021-08-22 19:18:31 +0900

    Updated setup.sh for light-mode

--- setup.sh
+++ setup.sh
@@ -5,4 +5,11 @@ headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
 \n\
+[theme]\n\
+primaryColor=\"#F63366\"\n\
+backgroundColor=\"#FFFFFF\"\n\
+secondaryBackgroundColor=\"#F0F2F6\"\n\
+textColor=\"#262730\"\n\
+font=\"sans serif\"\n\
+\n\
 " > ~/.streamlit/config.toml
